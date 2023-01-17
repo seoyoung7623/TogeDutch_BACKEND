54532c94ffe4c1fbf03b2ee85d17d39c76c15c37
aeeazip <codnjs0809@naver.com>
2023-01-17 10:44:44 +0900

    add : 공고 관심목록에 담기

--- TogeDutch/src/main/java/com/proj/togedutch/dao/LikeUsersDao.java
+++ TogeDutch/src/main/java/com/proj/togedutch/dao/LikeUsersDao.java
@@ -1,5 +1,7 @@
 package com.proj.togedutch.dao;
 
+import com.proj.togedutch.entity.LikeUsers;
+import com.proj.togedutch.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,31 @@ public class LikeUsersDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    // post 등록한 user_id 반환
+    public int findUploader(int postIdx){
+        String findUploaderQuery = "select User_user_id from Post where post_id = ?";
+        int uploader = this.jdbcTemplate.queryForObject(findUploaderQuery, new Object[]{postIdx}, Integer.class);
+
+        return uploader;
+    }
+
+    public int createLikePost(int userIdx, int postIdx, int Uploader_userIdx){
+        String createLikePostQuery
+                = "insert into LikeUsers(Post_post_id, Post_User_user_id, User_user_id) VALUES (?, ?, ?)";
+        Object[] createLikePostParams = new Object[]{postIdx, Uploader_userIdx, userIdx};
+        this.jdbcTemplate.update(createLikePostQuery, createLikePostParams);
+        String lastInsertIdQuery = "select last_insert_id()";
+        return this.jdbcTemplate.queryForObject(lastInsertIdQuery, int.class);
+    }
+
+    public LikeUsers getLikePostById(int likeIdx){
+        String getLikePostQuery = "select * from LikeUsers where like_id = ?";
+        return this.jdbcTemplate.queryForObject(getLikePostQuery,
+                (rs, rowNum) -> new LikeUsers(
+                        rs.getInt("like_id"),
+                        rs.getInt("Post_post_id"),
+                        rs.getInt("Post_User_user_id"),
+                        rs.getInt("User_user_id")
+                ), likeIdx);
+    }
 }
--- TogeDutch/src/main/java/com/proj/togedutch/service/LikeUsersService.java
+++ TogeDutch/src/main/java/com/proj/togedutch/service/LikeUsersService.java
@@ -1,11 +1,17 @@
 package com.proj.togedutch.service;
 
+import com.proj.togedutch.config.BaseException;
+import com.proj.togedutch.config.BaseResponse;
 import com.proj.togedutch.dao.LikeUsersDao;
+import com.proj.togedutch.entity.LikeUsers;
+import com.proj.togedutch.entity.Post;
 import com.proj.togedutch.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.proj.togedutch.config.BaseResponseStatus.DATABASE_ERROR;
 
 @Service
 public class LikeUsersService {
@@ -15,4 +21,17 @@ public class LikeUsersService {
 
     @Autowired
     JwtService jwtService;
+
+    public LikeUsers createLikePost(int userIdx, int postIdx) throws BaseException {
+        try {
+            int Uploader_user_id = likeUsersDao.findUploader(postIdx);
+            logger.info("Uploader_id : " + Uploader_user_id);
+
+            int likeIdx = likeUsersDao.createLikePost(userIdx, postIdx, Uploader_user_id);
+            LikeUsers likeUsers = likeUsersDao.getLikePostById(likeIdx);
+            return likeUsers;
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
 }
