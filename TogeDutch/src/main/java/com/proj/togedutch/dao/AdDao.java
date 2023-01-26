edb58e7e62c70f23a820cd41ba276c589fc97eee
mine <junghee0204@naver.com>
2023-01-27 02:23:19 +0900

    feat: 광고 CRUD 완료 #mine

--- TogeDutch/src/main/java/com/proj/togedutch/dao/AdDao.java
+++ TogeDutch/src/main/java/com/proj/togedutch/dao/AdDao.java
@@ -1,8 +1,6 @@
 package com.proj.togedutch.dao;
 
 import com.proj.togedutch.entity.Advertisement;
-import com.proj.togedutch.entity.Post;
-import com.proj.togedutch.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class AdDao {
@@ -25,7 +24,7 @@ public class AdDao {
     @Transactional(rollbackFor = Exception.class)
     public int createAd(Advertisement ad, int userIdx, String fileUrl) {
         String createAdQuery
-                = "insert into Advertisement (store, information, main_menu, delivery_tips, location, request, User_user_id, FileProvider_file_id) " +
+                = "insert into Advertisement (store, information, main_menu, delivery_tips, location, request, User_user_id, image) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         Object[] createAdParams = new Object[]{ad.getStore(), ad.getInformation(), ad.getMainMenu(), ad.getDeliveryTips(), ad.getLocation(), ad.getRequest(), userIdx, fileUrl};
         this.jdbcTemplate.update(createAdQuery, createAdParams);
@@ -48,8 +47,46 @@ public class AdDao {
                         rs.getTimestamp("updated_at"),
                         rs.getString("status"),
                         rs.getInt("User_user_id"),
-                        rs.getInt("FileProvider_file_id")
+                        rs.getString("image")
                 ), adIdx);
+    }
+
+    public List<Advertisement> getAdsByUserId(int userIdx) {
+        String getAdsQuery = "select * from Advertisement where User_user_id = ?";
+        return this.jdbcTemplate.query(getAdsQuery,
+                (rs, rowNum) -> new Advertisement(
+                        rs.getInt("ad_id"),
+                        rs.getString("store"),
+                        rs.getString("information"),
+                        rs.getString("main_menu"),
+                        rs.getInt("delivery_tips"),
+                        rs.getString("location"),
+                        rs.getString("request"),
+                        rs.getTimestamp("created_at"),
+                        rs.getTimestamp("updated_at"),
+                        rs.getString("status"),
+                        rs.getInt("User_user_id"),
+                        rs.getString("image")
+                ), userIdx);
+    }
+
+    public Advertisement getRandomAd() {
+        String getRandAd = "Select * From Advertisement Order by rand() Limit 1";
+        return this.jdbcTemplate.queryForObject(getRandAd,
+                (rs, rowNum) -> new Advertisement(
+                        rs.getInt("ad_id"),
+                        rs.getString("store"),
+                        rs.getString("information"),
+                        rs.getString("main_menu"),
+                        rs.getInt("delivery_tips"),
+                        rs.getString("location"),
+                        rs.getString("request"),
+                        rs.getTimestamp("created_at"),
+                        rs.getTimestamp("updated_at"),
+                        rs.getString("status"),
+                        rs.getInt("User_user_id"),
+                        rs.getString("image")
+                ));
     }
 
 }
