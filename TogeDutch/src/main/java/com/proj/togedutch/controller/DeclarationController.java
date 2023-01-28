a3f0ae4a0a41250e4cda1f1186700f69b806311a
hyeonah <hyeona0000@naver.com>
2023-01-28 18:57:29 +0900

    add: declaration mvc create, get #conan

--- TogeDutch/src/main/java/com/proj/togedutch/controller/DeclarationController.java
+++ TogeDutch/src/main/java/com/proj/togedutch/controller/DeclarationController.java
@@ -1,7 +1,25 @@
 package com.proj.togedutch.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.proj.togedutch.config.BaseException;
+import com.proj.togedutch.config.BaseResponse;
+import com.proj.togedutch.entity.Declaration;
+import com.proj.togedutch.service.DeclarationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DeclarationController {
+    @Autowired
+    DeclarationService declarationService;
+
+    @ResponseBody
+    @PostMapping(value = "/declaration/{chatRoomIdx}")
+    public BaseResponse<Declaration> createDeclaration(@PathVariable("chatRoomIdx") int chatRoomIdx, @RequestBody Declaration declaration) {
+        try {
+            Declaration de = declarationService.createDeclaration(declaration, chatRoomIdx);
+            return new BaseResponse<>(de);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 }
--- TogeDutch/src/main/java/com/proj/togedutch/service/DeclarationService.java
+++ TogeDutch/src/main/java/com/proj/togedutch/service/DeclarationService.java
@@ -1,7 +1,32 @@
 package com.proj.togedutch.service;
 
+import com.proj.togedutch.config.BaseException;
+import com.proj.togedutch.config.BaseResponseStatus;
+import com.proj.togedutch.dao.DeclarationDao;
+import com.proj.togedutch.entity.Declaration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeclarationService {
+    @Autowired
+    DeclarationDao declarationDao;
+
+    public Declaration createDeclaration(Declaration declaration, int chatRoomIdx) throws BaseException {
+        try {
+            int declarationIdx = declarationDao.createDeclaration(declaration, chatRoomIdx);
+            Declaration newDeclaration = getDeclarationById(declarationIdx);
+            return newDeclaration;
+        } catch (Exception e) {
+            throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
+        }
+    }
+    public Declaration getDeclarationById(int declarationIdx) throws  BaseException {
+        try {
+            Declaration findDeclaration = declarationDao.getDeclarationById(declarationIdx);
+            return findDeclaration;
+        } catch (Exception e) {
+            throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
+        }
+    }
 }
