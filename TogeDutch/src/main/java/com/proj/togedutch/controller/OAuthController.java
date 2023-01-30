c6aabe101e39d8c130dba02afa350701e7c91d20
aeeazip <codnjs0809@naver.com>
2023-01-31 02:33:13 +0900

    docs : 로그 추가 #jay

--- TogeDutch/src/main/java/com/proj/togedutch/controller/OAuthController.java
+++ TogeDutch/src/main/java/com/proj/togedutch/controller/OAuthController.java
@@ -30,10 +30,13 @@ public class OAuthController {
         /*
             # return 타입 정리 #
 
-            0 : 일반 로그인 페이지로 redirect
+            0 : 일반 로그인 페이지로 redirect(카카오 로그인 이메일 동의 선택 안 한 경우)
             1 : 로그인 완료 (메인 페이지로 이동)
             -1 : 회원가입 페이지로 이동 (회원가입 페이지에 해당 email 값 띄워주기) -> return 값 변경 필요
          */
+
+        logger.info("email : " + email);      // email null이냐???
+        logger.info("userInfo.get(email) : " + userInfo.get(email));
 
         if(userInfo.get("email") == null) {  // 카카오 로그인 이메일 동의 선택 안한 경우
             logger.info("일반 로그인 페이지로 redirect");
