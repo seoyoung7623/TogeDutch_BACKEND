9ada3f927cb3be9fc9b00d2430b1dba4f7624616
eojin0814 <leeeojin6@gmail.com>
2023-02-12 03:07:23 +0900

    fix: review #jinny

--- TogeDutch/src/main/java/com/proj/togedutch/controller/ReviewController.java
+++ TogeDutch/src/main/java/com/proj/togedutch/controller/ReviewController.java
@@ -3,10 +3,7 @@ package com.proj.togedutch.controller;
 import com.proj.togedutch.config.BaseException;
 import com.proj.togedutch.config.BaseResponse;
 import com.proj.togedutch.config.BaseResponseStatus;
-import com.proj.togedutch.entity.Keyword;
-import com.proj.togedutch.entity.Post;
-import com.proj.togedutch.entity.Review;
-import com.proj.togedutch.entity.User;
+import com.proj.togedutch.entity.*;
 import com.proj.togedutch.service.AWSS3Service;
 import com.proj.togedutch.service.PostService;
 import com.proj.togedutch.service.ReviewService;
@@ -14,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -59,6 +57,16 @@ public class ReviewController {
         }
     }
 
+    @ResponseBody
+    @GetMapping("/emotion/{postId}")
+    public BaseResponse<ReviewEmotion> getEmotionReview(@PathVariable("postId") int postId) {
+        try {
+            ReviewEmotion getEmotion = reviewService.getEmotionReview(postId);
+            return new BaseResponse<>(getEmotion);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 
 
 }
